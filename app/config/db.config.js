40deeee2673408be32ee9e14fe78614c82dffd41
popnaphat <40283665+popnaphat@users.noreply.github.com>
2023-07-11 14:29:17 +0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
+  HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "b84178f5d1988d",
+  PASSWORD: "c3bbce1d",
   DB: "testdb"
 };
