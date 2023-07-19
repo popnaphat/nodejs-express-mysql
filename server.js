8349dcfe5c2b46da724c05acaa97cf6fc0add148
popnaphat <40283665+popnaphat@users.noreply.github.com>
2023-07-19 13:35:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://nodejsreact-544e74e48d27.herokuapp.com:8081/"
 };
 
 app.use(cors(corsOptions));
